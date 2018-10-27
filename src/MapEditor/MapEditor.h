eevee/SLADE

Initial support for drawing a selected 3D floor plane correctly

src/MapEditor/MapEditor.h
@@ -10,16 +10,24 @@ struct selection_3d_t
 {
 	int		index;
 	uint8_t	type;
+	int		extra_floor_index;
 
-	selection_3d_t(int index = -1, uint8_t type = 0)
+	selection_3d_t(int index = -1, uint8_t type = 0, int extra_floor_index = -1)
 	{
 		this->index = index;
 		this->type = type;
+		this->extra_floor_index = extra_floor_index;
 	}
 
-	bool operator<(const selection_3d_t& other) const {
+	bool operator<(const selection_3d_t& other) const
+	{
 		if (this->type == other.type)
-			return this->index < other.index;
+		{
+			if (this->index == other.index)
+				return this->extra_floor_index < other.extra_floor_index;
+			else
+				return this->index < other.index;
+		}
 		else
 			return this->type < other.type;
 	}

usernameak/GZDoomSLADE

src/MapEditor/MapEditor.h
@@ -51,19 +51,26 @@ namespace MapEditor
 	{
 		int			index;
 		ItemType	type;
+		int		    extra_floor_index;
 
-		Item(int index = -1, ItemType type = ItemType::Any) : index{ index }, type{ type } {}
+		Item(int index = -1, ItemType type = ItemType::Any, int extra_floor_index = -1) : index{ index }, type{ type }, extra_floor_index{ extra_floor_index } {}
 
 		// Comparison operators
-		bool operator<(const Item& other) const {
+		bool operator<(const Item& other) const
+		{
 			if (this->type == other.type)
-				return this->index < other.index;
+			{
+				if (this->index == other.index)
+					return this->extra_floor_index < other.extra_floor_index;
+				else
+					return this->index < other.index;
+			}
 			else
 				return this->type < other.type;
 		}
 		bool operator==(const Item& other) const
 		{
-			return index == other.index && (type == ItemType::Any || type == other.type);
+			return index == other.index && (type == ItemType::Any || type == other.type) && this->extra_floor_index == other.extra_floor_index;
 		}
 		bool operator!=(const Item& other) const
 		{