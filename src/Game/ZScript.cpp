77ad2255702a46ee18d15ebcb2567ab02d1c37a0
usernameak <sitemakers.3dn.ru@gmail.com>
2018-10-29 14:56:04 +0200

    Fixed 'Assertion failed' on ZScript

--- src/Game/ZScript.cpp
+++ src/Game/ZScript.cpp
@@ -969,7 +969,7 @@ bool Definitions::parseZScript(ArchiveEntry* entry)
 	auto start = App::runTimer();
 	vector<ParsedStatement> parsed;
 	parseBlocks(entry, parsed);
-	Log::debug(2, S_FMT("parseBlocks: %dms", App::runTimer() - start));
+	Log::debug(2, S_FMT("parseBlocks: %ldms", App::runTimer() - start));
 	start = App::runTimer();
 
 	for (auto& block : parsed)
@@ -1027,7 +1027,7 @@ bool Definitions::parseZScript(ArchiveEntry* entry)
 		}
 	}
 
-	Log::debug(2, S_FMT("ZScript: %dms", App::runTimer() - start));
+	Log::debug(2, S_FMT("ZScript: %ldms", App::runTimer() - start));
 
 	return true;
 }
